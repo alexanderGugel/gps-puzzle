958db15527bad83701a8834841ea5fd634a56595
Alexander Gugel <alexander.gugel@gmail.com>
2014-10-17 13:32:50 +0200

    Complete

--- app.js
+++ app.js
@@ -3,19 +3,15 @@ var _ = require('underscore');
 
 var R = 6373; // Radius of the earth in km
 
-var time = function (point1, point2) {
-  return (point2[2] - point1[2])*24;
-};
-
 var deg2rad = function (deg) {
   return deg * (Math.PI/180)
 };
 
 var distance = function (point1, point2) {
-  var lat1 = point1[0];
-  var lon1 = point1[1];
-  var lat2 = point2[0];
-  var lon2 = point2[1];
+  var lat1 = point1.latitude;
+  var lon1 = point1.longitude;
+  var lat2 = point2.latitude;
+  var lon2 = point2.longitude;
 
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2-lat1);
@@ -31,66 +27,100 @@ var distance = function (point1, point2) {
   return distance;
 };
 
+var file = fs.readFileSync('data.txt', 'utf8');
+
+var data = _.map(file.split('\n'), function (line) {
+  return line.split(' ');
+});
+
+var points = data.slice(0, data.length-1); // remove empty line
+
+points = _.map(points, function (point, index) {
+  return {
+    latitude: point[0],
+    longitude: point[1],
+    timestamp: point[2]
+  };
+});
+
+_.each(points, function (point, index) {
+  point.diffDistance = distance(points[index-1] || point, point);
+  point.diffTime = (point.timestamp - (points[index-1] || point).timestamp) * 24;
+});
+
+// In km
 var totalDistance = function (points) {
-  var totalDistance = 0;
-  for (var i = 0; i < points.length - 1; i++) {
-    var current = points[i];
-    totalDistance += distance(points[i], points[i+1]);
-  }
-  return totalDistance;
+  return _.reduce(points, function (totalDistance, point) {
+    return totalDistance += point.diffDistance;
+  }, 0);
 };
 
+// In h
 var totalTime = function (points) {
-  var totalTime = 0;
-  for (var i = 0; i < points.length - 1; i++) {
-    var current = points[i];
-    totalTime += time(points[i], points[i+1]);
-  }
-  return totalTime;
+  return _.reduce(points, function (totalTime, point) {
+    return totalTime += point.diffTime;
+  }, 0);
 };
 
-var throttle = function (points, speed) {
+// In km/h
+var averageSpeed = function (points) {
+  return (totalDistance(points) / totalTime(points));
+};
+
+// Output all the info
+var info = function (points) {
+  console.log('Average speed: ' + averageSpeed(points) + ' km/h');
+  console.log('Total time: ' + totalTime(points)*60 + ' min');
+  console.log('Total distance: ' + totalDistance(points) + ' km');
+};
+
+var throttle = function (points, maxSpeed) {
+  // Clone array and objects within in
   points = points.slice();
+  points = _.map(points, function (point) {
+    return _.clone(point);
+  });
+
+  _.each(points, function (point, index) {
+    if (point.diffTime !== 0) {
+      var speed = (point.diffDistance / point.diffTime);
+      if (speed > maxSpeed) {
+        speed = maxSpeed;
+        point.diffTime = point.diffDistance / maxSpeed;
+      }
+    }
+  });
 
   return points;
 };
 
-var info = function (points) {
-  var distance = totalDistance(points);
-  var time = totalTime(points);
-  var speed = distance/time;
-
-  console.log('Total distance: ' + distance + ' km');
-  console.log('Total time: ' + time*60 + ' min');
-  console.log('Average speed: ' + speed + ' km/h')
-};
-
-var decorateSpeed = function (points) {
-  for (var i = 0; i < points.length - 1; i++) {
-    points[i+1][3] = distance(points[i], points[i+1]) / time(points[i], points[i+1]);
-  }
-};
-
-var decorateTimeDiff = function (points) {
-  for (var i = 0; i < points.length - 1; i++) {
-    points[i+1][3] = points[i+1] - points[i+1];
-  }
-};
-
-var file = fs.readFileSync('data.txt', 'utf8');
-var data = _.map(file.split('\n'), function (line) {
-  return line.split(' ');
-});
-var points = data.slice(0, data.length-1);
-
-decorateSpeed(points);
-console.log(points);
-
-console.log('\nStandard: ');
+console.log('\nNo throttling');
 info(points);
 
-console.log('\nThrottle 35: ');
+console.log('\nMax speed 30 km/h');
+info(throttle(points, 30));
+
+console.log('\nMax speed 35 km/h');
 info(throttle(points, 35));
 
-console.log('\nThrottle 30: ');
-info(throttle(points, 30));
+
+
+
+// In min
+// console.log(totalTime(points)*60);
+
+// Total distance: 6.813706487270152 km
+// Total time: 14.299200000241399 min
+// Average speed: 28.59057774066433 km/h
+
+// decorateSpeed(points);
+// console.log(points);
+//
+// console.log('\nStandard: ');
+// info(points);
+//
+// console.log('\nThrottle 35: ');
+// info(throttle(points, 35));
+//
+// console.log('\nThrottle 30: ');
+// info(throttle(points, 30));
